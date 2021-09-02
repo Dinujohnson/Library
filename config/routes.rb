7d9953a6897c9d2a9540a7ec7df39d8e9b62a596
dinu <dinujohnson726@gmail.com>
2021-09-03 00:01:39 +0530

    Second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+get "library/students"
   resources :students
   resources :books
+  
+  root to: 'students#index'  
+
+  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+

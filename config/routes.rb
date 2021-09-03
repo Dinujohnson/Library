3ed6be7fe9576abe2341a387ced09cff90f13d2c
dinu <dinujohnson726@gmail.com>
2021-09-04 02:07:28 +0530

    Third Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-get "library/students"
+
   resources :students
   resources :books
   
   root to: 'students#index'  
+ 
+ #root to represents what do you want the home page to be
+  #Above students represent controller then comes the pounds sign which is '#' which says the the next thing provided will be the method 
+  
 
   
 
